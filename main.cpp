6e6e9f431cbdd46e34f9fe835f5d964282f61e85
Nadezhda <nadezhdadieslast@gmail.com>
2024-06-25 21:27:55 +0400

    The function returns true if the second string is a substring of the first

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+bool findSubStr(const char* text, const char* word) {
+    for (int i = 0; i < strlen(text); i++) {
+        for (int j = 0; j < i; j++) {
+            if (*(text + i) == *(word + j)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    const char* text = "Hello world!";
+    const char* word = "wor";
+    const char* word2 = "el";
+//    if (findSubStr(text, word)) {
+//        std::cout << "Yes";
+//    } else {
+//        std::cout << "No";
+//    }
+    std::cout << findSubStr(text, word) << std::endl;
+    std::cout << findSubStr(text, word2) << std::endl;
+
+    return 0;
+}
