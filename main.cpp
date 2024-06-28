584f407b042db5bd3c2665cd2c1d86b8fc9d87e4
Nadezhda <nadezhdadieslast@gmail.com>
2024-06-28 18:18:28 +0400

    already found substring

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include <cstring>
 
-bool findSubStr(const char* text, const char* word) {
+bool findSubStr(const char *text, const char *word) {
+    int counter = 0;
     for (int i = 0; i < strlen(text); i++) {
-        for (int j = 0; j < i; j++) {
-            if (*(text + i) == *(word + j)) {
+        for (int j = 0; j < strlen(word); j++) {
+            if (*(text + i + j) == *(word + j)) {
+                counter++;
+            }
+            if (strlen(word) == counter) {
                 return true;
+            }
+            if (*(text + i + j) != *(word + j)) {
+                counter = 0;
+                break;
             }
         }
     }
+
     return false;
 }
 
 int main() {
-    const char* text = "Hello world!";
-    const char* word = "wor";
-    const char* word2 = "el";
+    const char *text = "Hello world!";
+    const char *word = "banana";
+    const char *word2 = "Hello";
+    const char *word3 = "lo";
+
 
     std::cout << findSubStr(text, word) << std::endl;
     std::cout << findSubStr(text, word2) << std::endl;
+    std::cout << findSubStr(text, word3) << std::endl;
 
     return 0;
 }
